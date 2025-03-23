31cee9209fed49521996b9af7e75c28335bae382
Gal-Schwartz <gal1042000@gmail.com>
2025-03-23 10:06:53 +0200

    Validation

---
+++ popcorn-palace/src/main/java/com/att/tdp/popcorn_palace/validation/ValidReleaseYear.java
@@ -0,0 +1,23 @@
+
+
+// ======= Custom annotation for release year =======
+package com.att.tdp.popcorn_palace.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = ReleaseYearValidator.class)
+public @interface ValidReleaseYear {
+    String message() default "Release year must be between 1900 and the current year";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
+
+
+
+
+
+
